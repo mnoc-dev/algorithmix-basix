b3ee0e77102cac6b81e5944f448f3ca8a76cc7ef
mnoc-dev <matthieuruel60@gmail.com>
2026-06-21 18:49:06 +0200

    replace added

--- replace/run.js
+++ replace/run.js
@@ -1,8 +1,23 @@
 // Do not change, helper to read arguments:
 const args = process.argv.slice(2);
-
 // Implement below:
-const output = null;
+console.log(args);
+let output = "";
+let mot = args[0];
+let supprimer =  args[1]; //lettre o
+let remplacer = args[2]; //lettre a
+
+for (let i = 0; i < mot.length; i++)
+{
+	if (mot[i] === supprimer){
+	output += remplacer;
+	//t'arrives à O  du coup prends ce qu'il y avant  x + y(nouvelle partie du calc
+	}else
+	{ 
+	output += mot[i];
+	}
+}
 
 // Do not change:
 console.log(output);
+
